f08b8ec41ab44de70db2ac1cb15921eebfee61cc
Michael Kaufman <mkaufman94@gmail.com>
2022-01-15 10:29:27 -0600

    feat: Implement setup function

--- src/hooks/useRecorder.js
+++ src/hooks/useRecorder.js
@@ -1,13 +1,35 @@
 import { useState, useEffect } from 'react';
-import recorder from '../utilities/recorder';
+import Recorder from '../utilities/recorder';
+
+const { URL, Blob, Promise, navigator, FileReader, AudioContext } = window;
 
 const useRecorder = () => {
+  const [recorder, setRecorder] = useState();
+  const [audioContext, setAudioContext] = useState();
   const [audioBlob, setAudioBlob] = useState();
   const [audioURL, setAudioURL] = useState();
 
+  const audioElementType = 'audio/wav';
+
+  const createNewRecorder = async (stream) => {
+    const input = audioContext.createMediaStreamSource(stream);
+    const newRecorder = new Recorder(input);
+    setRecorder(newRecorder);
+  };
+
+  const setup = async () => {
+    if (!audioContext) {
+      setAudioContext(new AudioContext());
+    }
+
+    return navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      createNewRecorder(stream);
+    });
+  };
+
   useEffect(() => {}, []);
 
-  return { audioBlob, audioURL };
+  return { audioBlob, audioURL, setup };
 };
 
 export default useRecorder;
